121d32a7b1158fcce9df42c843e6e69b7a12102d
zhoujj <1254883879@qq.com>
2019-03-04 06:28:49 +0800

    初始化

---
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 120);
+            $table->rememberToken();//为用户创建一个 remember_token 字段，用于保存『记住我』的相关信息。
+            $table->timestamps();//创建了一个 created_at 和一个 updated_at 字段，分别用于保存用户的创建时间和更新时间。
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ blog/resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="">
+            <a href="htpps://l.com">学无止境</a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="/about">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
